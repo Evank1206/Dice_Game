3984d5851e4d525fba916866dbaa1701827e18c3
Jatinokov <Enkhtaivan1206@gmail.com>
2020-06-06 02:04:19 -0700

    app.js : created two functions for each player, and using eventListener for get the img from html

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let mainScore = [0, 0]
 // variable for temprery score
 let currentScore = 0;
 
+
+
 document.querySelector(".scroll").addEventListener("click", function () {
     // console.log("hey clicked");
     // dice random numbers 1-6
@@ -12,7 +14,18 @@ document.querySelector(".scroll").addEventListener("click", function () {
     let dice = Math.floor(Math.random() * 6) + 1;
     // console.log(dice);
     // to dom current score player one
-    document.querySelector(".currentH1").textContent = dice;
+    let x = document.querySelector(".currentH1").textContent = dice;
+    console.log(x);
+ document.querySelector(".dice").src = "one"+x+".png"
+    switch (true) {
+        case x === 1: one; break;
+        case x === 2: two; break;
+        case x === 3: three; break;
+        case x === 4: four; break;
+        case x === 5: five; break;
+        case x === 6: six; break;
+    }
+
 
 });
 
