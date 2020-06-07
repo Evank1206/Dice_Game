a4f8f5b836fc077e0dbad1e2fa6998f4087908ae
Jatinokov <Enkhtaivan1206@gmail.com>
2020-06-06 17:09:59 -0700

    app.js: created global variable for currentDice number and each time click the scroll button increments by random dice numbers, get dice image by className and gave 'src' attribute with concatenate the random dice number Which give result of display different images matched with dice number

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ let mainScore = [0, 0]
 // variable for temprery score
 let currentScore = 0;
 
-
+document.querySelector(".savedScore").textContent = "1";
+document.querySelector(".dice").style.display = "none";
+let currentScoreIncrement = 0;
 
 document.querySelector(".scroll").addEventListener("click", function () {
     // console.log("hey clicked");
     // dice random numbers 1-6
-
-    let dice = Math.floor(Math.random() * 6) + 1;
-    // console.log(dice);
+    let diceNum = Math.floor(Math.random() * 6) + 1;
+    // console.log(diceNum);
+    currentScoreIncrement = currentScoreIncrement + diceNum;
+    
     // to dom current score player one
-    let x = document.querySelector(".currentH1").textContent = dice;
-    console.log(x);
- document.querySelector(".dice").src = "one"+x+".png"
-    switch (true) {
-        case x === 1: one; break;
-        case x === 2: two; break;
-        case x === 3: three; break;
-        case x === 4: four; break;
-        case x === 5: five; break;
-        case x === 6: six; break;
-    }
-
+    let diceImage = document.querySelector(".currentH1").textContent = currentScoreIncrement;
+    document.querySelector(".savedScore").textContent = "0";
+    // console.log(x);
+    document.querySelector(".dice").src = "img-" + diceImage + ".png";
+    document.querySelector(".dice").style.display = "block";
+   
+ 
+ 
 
 });
 
