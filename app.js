5243062700cacbef90da6b8e85ce74fba136c67c
Jatinokov <Enkhtaivan1206@gmail.com>
2020-06-08 22:41:38 -0700

    app.js: created conditional for switching users and fixed the bug which is not adding the  main score in players board

--- app.js
+++ app.js
@@ -4,49 +4,48 @@ let activePlayer = 0;
 let roundScore = [0, 0]
 // variable for temprery score
 let currentScore = 0;
-
-document.querySelector(".savedScore").textContent = "1";
+// player 0 is the head to start game
+document.querySelector(".savedScore-" + activePlayer).textContent = currentScore;
 document.querySelector(".dice").style.display = "none"; // hiding the dice
-// let currentScoreIncrement = 0;
-
+// dice Rolling function
 document.querySelector(".scroll").addEventListener("click", function () {
     let diceNumber = Math.floor(Math.random() * 6) + 1; // get random dice number 1-6
-    console.log(diceNumber);
-    document.querySelector(".dice").src = "./img/" + "img-" + diceNumber + ".png"; // displaying diceImage
+    // console.log(diceNumber);
     document.querySelector(".dice").style.display = "block"; // displaying dice
-    // document.querySelector(".savedScore").textContent = "7"; // displayig game Score
+    document.querySelector(".dice").src = "./img/" + "img-" + diceNumber + ".png"; // displaying diceImage
+
     // condtional: accept number-"1", add random numbers and display the numbers in current score section
     if (diceNumber !== 1) {
         currentScore = currentScore + diceNumber;  // add random number to in currentScore
         document.getElementById("current-" + activePlayer).textContent = currentScore; // to DOM added currenScore
-        holdFunction();
     } else { // if diceNumber === 1 is erase the current score to 0
         currentScore = 0;
-        document.getElementById("current-" + activePlayer).innerHTML = currentScore;  // erase the current score
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // if active player dice equal to random number-"0" switch to player two, else switch to player one
+        document.getElementById("current-" + activePlayer).textContent = currentScore;  // erase the current score
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // if active player = "0" playerOne, switch to playerTwo, else switch to player one
         document.getElementById("active-0").classList.toggle("active");
         document.getElementById("active-1").classList.toggle("active");
     }
+});
+
+document.querySelector(".hold").addEventListener("click", function () {
+   activePlayer === 0 ? switchPlayer() : switchPlayer();
 
 });
 
-document.querySelector(".hold").addEventListener("click", holdFunction);
+function switchPlayer() {
+    // add the current score to round score
+    roundScore[activePlayer] = roundScore[activePlayer] + currentScore;
+    document.querySelector(".savedScore-" + activePlayer).textContent = roundScore[activePlayer];
 
-function holdFunction() {
-    currentScore.push(roundScore[0])
+    // erase the current score
+    currentScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = currentScore; // erased current score from DOM
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // if active player dice equal to random number-"0" switch to player two, else switch to player one
+    // THE SMALL DOT switch player two
+    document.getElementById("active-0").classList.toggle("active");
+    document.getElementById("active-1").classList.toggle("active");
+    //  erase the dice from DOM
+    document.querySelector(".dice").style.display = "none";
 
 }
-
-
-
-
-
-// document.querySelector(".scroll").addEventListener("click", function () {
-
-//     let dice = Math.floor(Math.random() * 6) + 1;
-//     // to dom current score player two
-//     document.querySelector(".currentH2").textContent = dice;
-
-// })
-
 
